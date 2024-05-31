eaec2e936b6293a1217706f28c62d75a363e66d4
Daniel Necesal <daniel.necesal@mindmaze.ch>
2024-05-31 21:19:54 +0200

    Refactored how we handle setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="quick_coding",
     version="1.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
 )
